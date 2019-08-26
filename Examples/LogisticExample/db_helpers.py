4d4f6cbc8ed4c3bb0c0639e493b4b2e3adc9fc00
John Mount <jmount@win-vector.com>
2019-08-26 11:54:24 -0700

    move fns to file

---
+++ Examples/LogisticExample/db_helpers.py
@@ -0,0 +1,36 @@
+def is_numeric(col):
+    try:
+        0.0 + col
+        return True
+    except Exception as ex:
+        return False
+
+
+def insert_table(conn, d, table_name):
+    cr = [
+        d.columns[i].lower()
+        + " "
+        + ("double precision" if is_numeric(d[d.columns[i]]) else "VARCHAR")
+        for i in range(d.shape[1])
+    ]
+    table_name = "d"
+    create_stmt = "CREATE TABLE " + table_name + " ( " + ", ".join(cr) + " )"
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS " + table_name)
+    conn.commit()
+    cur.execute(create_stmt)
+    conn.commit()
+    buf = io.StringIO(d.to_csv(index=False, header=False, sep="\t"))
+    cur.copy_from(buf, "d", columns=[c for c in d.columns])
+    conn.commit()
+
+
+def read_query(conn, q):
+    cur.execute(q)
+    r = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+    return pandas.DataFrame(columns=colnames, data=r)
+
+
+def read_table(conn, table_name):
+    return read_query(conn, "SELECT * FROM " + table_name)
