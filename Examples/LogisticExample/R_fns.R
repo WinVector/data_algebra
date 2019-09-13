b5bdaa07c2e9bf923cdbdf3db1c99b343bea0018
John Mount <jmount@win-vector.com>
2019-09-13 04:04:48 -0700

    fix R fns

--- Examples/LogisticExample/R_fns.R
+++ Examples/LogisticExample/R_fns.R
@@ -20,7 +20,7 @@ convert_yaml_to_pipeline <- function(rep, source=NULL) {
       return(mk_td(table_name = rep$table_name,
                    columns = rep$column_names))
     } else if(op=="Extend") {
-      return(extend_se(source, 
+      return(extend_se(source,
                        assignments = expr_map_to_expr_array(rep$ops),
                        partitionby = as.character(rep$partition_by),
                        orderby = as.character(rep$order_by),
@@ -35,41 +35,7 @@ convert_yaml_to_pipeline <- function(rep, source=NULL) {
     } else if(op=="Order") {
       return(orderby(source, cols=rep$order_columns, reverse=rep$reverse, limit=rep$limit))
     } else {
-      stop("Unexpected node type: " + op) 
-    }
-  }
-} <- function(rep, source=NULL) {
-  if(is.null(names(rep))) {
-    # unnamed list, a pipeline
-    res <- convert_yaml_to_pipeline(rep[[1]])
-    for(i in seqi(2, length(res))) {
-      res <- convert_yaml_to_pipeline(rep[[i]], source=res)
-    }
-    return(res)
-  } else {
-    # named list, a stage
-    op = rep$op
-    if(op=="TableDescription") {
-      return(mk_td(table_name = rep$table_name,
-                   columns = rep$column_names))
-    } else if(op=="Extend") {
-      return(extend_se(source, 
-                       assignments = expr_map_to_expr_array(rep$ops),
-                       partitionby = as.character(rep$partition_by),
-                       orderby = as.character(rep$order_by),
-                       reverse = as.character(rep$reverse)))
-    } else if(op=="SelectRows") {
-      return(select_rows_se(source,
-                            expr = rep$expr))
-    } else if(op=="SelectColumns") {
-      return(select_columns(source, columns=rep$columns))
-    } else if(op=="Rename") {
-      return(rename_columns(source, cmap=rep$column_remapping))  # TODO: see if map is dumped (or __repr__() call wrote a string)
-    } else if(op=="Order") {
-      return(orderby(source, cols=rep$order_columns, reverse=rep$reverse, limit=rep$limit))
-    } else {
-      stop("Unexpected node type: " + op) 
+      stop("Unexpected node type: " + op)
     }
   }
 }
-
