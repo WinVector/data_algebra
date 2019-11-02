c84faff4c5d8213d8434b184297183fab50e72b7
John Mount <jmount@win-vector.com>
2019-11-02 07:01:57 -0700

    extend tests a bit

---
+++ tests/test_spark_sql.py
@@ -0,0 +1,17 @@
+
+import pandas
+from data_algebra.data_ops import *  # https://github.com/WinVector/data_algebra
+import data_algebra.SparkSQL
+import data_algebra.util
+
+
+def test_spark_sql():
+    ops = TableDescription(
+        "stocks", ["date", "trans", "symbol", "qty", "price"]
+    ).extend({"cost": "qty * price"})
+
+    pp = ops.to_python(pretty=True)
+
+    db_model = data_algebra.SparkSQL.SparkSQLModel()
+
+    sql = ops.to_sql(db_model, pretty=True)
