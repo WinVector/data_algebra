715ab1745951efbddd37f1415105aaae770c3400
John Mount <jmount@win-vector.com>
2020-01-31 12:23:25 -0800

    add iris exploding graph example

--- tests/test_cdata1.py
+++ tests/test_cdata1.py
@@ -167,3 +167,143 @@ def test_cdata1():
 
     recovered_transform = record_map_from_simple_obj(obj)
     waste_str = str(recovered_transform)
+
+
+def test_cdata_explode():
+    # from https://github.com/WinVector/cdata/blob/master/README.Rmd
+    control = data_algebra.pd.read_csv(io.StringIO(re.sub("[ \\t]+", "", """
+                v1,v2,value1,value2
+                Sepal.Length,Sepal.Length,Sepal.Length,Sepal.Length
+                Sepal.Width,Sepal.Length,Sepal.Width,Sepal.Length
+                Petal.Length,Sepal.Length,Petal.Length,Sepal.Length
+                Petal.Width,Sepal.Length,Petal.Width,Sepal.Length
+                Sepal.Length,Sepal.Width,Sepal.Length,Sepal.Width
+                Sepal.Width,Sepal.Width,Sepal.Width,Sepal.Width
+                Petal.Length,Sepal.Width,Petal.Length,Sepal.Width
+                Petal.Width,Sepal.Width,Petal.Width,Sepal.Width
+                Sepal.Length,Petal.Length,Sepal.Length,Petal.Length
+                Sepal.Width,Petal.Length,Sepal.Width,Petal.Length
+                Petal.Length,Petal.Length,Petal.Length,Petal.Length
+                Petal.Width,Petal.Length,Petal.Width,Petal.Length
+                Sepal.Length,Petal.Width,Sepal.Length,Petal.Width
+                Sepal.Width,Petal.Width,Sepal.Width,Petal.Width
+                Petal.Length,Petal.Width,Petal.Length,Petal.Width
+                Petal.Width,Petal.Width,Petal.Width,Petal.Width
+                """)))
+    iris = data_algebra.pd.read_csv(io.StringIO(re.sub("[ \\t]+", "", """
+                Sepal.Length,Sepal.Width,Petal.Length,Petal.Width,Species,iris_id
+                5.1,3.5,1.4,0.2,setosa,1
+                4.9,3,1.4,0.2,setosa,2
+                4.7,3.2,1.3,0.2,setosa,3
+                4.6,3.1,1.5,0.2,setosa,4
+                5,3.6,1.4,0.2,setosa,5
+                5.4,3.9,1.7,0.4,setosa,6
+    """)))
+    expect = data_algebra.pd.read_csv(io.StringIO(re.sub("[ \\t]+", "", """
+                iris_id,Species,v1,v2,value1,value2
+                1,setosa,Sepal.Length,Sepal.Length,5.1,5.1
+                1,setosa,Sepal.Width,Sepal.Length,3.5,5.1
+                1,setosa,Petal.Length,Sepal.Length,1.4,5.1
+                1,setosa,Petal.Width,Sepal.Length,0.2,5.1
+                1,setosa,Sepal.Length,Sepal.Width,5.1,3.5
+                1,setosa,Sepal.Width,Sepal.Width,3.5,3.5
+                1,setosa,Petal.Length,Sepal.Width,1.4,3.5
+                1,setosa,Petal.Width,Sepal.Width,0.2,3.5
+                1,setosa,Sepal.Length,Petal.Length,5.1,1.4
+                1,setosa,Sepal.Width,Petal.Length,3.5,1.4
+                1,setosa,Petal.Length,Petal.Length,1.4,1.4
+                1,setosa,Petal.Width,Petal.Length,0.2,1.4
+                1,setosa,Sepal.Length,Petal.Width,5.1,0.2
+                1,setosa,Sepal.Width,Petal.Width,3.5,0.2
+                1,setosa,Petal.Length,Petal.Width,1.4,0.2
+                1,setosa,Petal.Width,Petal.Width,0.2,0.2
+                2,setosa,Sepal.Length,Sepal.Length,4.9,4.9
+                2,setosa,Sepal.Width,Sepal.Length,3,4.9
+                2,setosa,Petal.Length,Sepal.Length,1.4,4.9
+                2,setosa,Petal.Width,Sepal.Length,0.2,4.9
+                2,setosa,Sepal.Length,Sepal.Width,4.9,3
+                2,setosa,Sepal.Width,Sepal.Width,3,3
+                2,setosa,Petal.Length,Sepal.Width,1.4,3
+                2,setosa,Petal.Width,Sepal.Width,0.2,3
+                2,setosa,Sepal.Length,Petal.Length,4.9,1.4
+                2,setosa,Sepal.Width,Petal.Length,3,1.4
+                2,setosa,Petal.Length,Petal.Length,1.4,1.4
+                2,setosa,Petal.Width,Petal.Length,0.2,1.4
+                2,setosa,Sepal.Length,Petal.Width,4.9,0.2
+                2,setosa,Sepal.Width,Petal.Width,3,0.2
+                2,setosa,Petal.Length,Petal.Width,1.4,0.2
+                2,setosa,Petal.Width,Petal.Width,0.2,0.2
+                3,setosa,Sepal.Length,Sepal.Length,4.7,4.7
+                3,setosa,Sepal.Width,Sepal.Length,3.2,4.7
+                3,setosa,Petal.Length,Sepal.Length,1.3,4.7
+                3,setosa,Petal.Width,Sepal.Length,0.2,4.7
+                3,setosa,Sepal.Length,Sepal.Width,4.7,3.2
+                3,setosa,Sepal.Width,Sepal.Width,3.2,3.2
+                3,setosa,Petal.Length,Sepal.Width,1.3,3.2
+                3,setosa,Petal.Width,Sepal.Width,0.2,3.2
+                3,setosa,Sepal.Length,Petal.Length,4.7,1.3
+                3,setosa,Sepal.Width,Petal.Length,3.2,1.3
+                3,setosa,Petal.Length,Petal.Length,1.3,1.3
+                3,setosa,Petal.Width,Petal.Length,0.2,1.3
+                3,setosa,Sepal.Length,Petal.Width,4.7,0.2
+                3,setosa,Sepal.Width,Petal.Width,3.2,0.2
+                3,setosa,Petal.Length,Petal.Width,1.3,0.2
+                3,setosa,Petal.Width,Petal.Width,0.2,0.2
+                4,setosa,Sepal.Length,Sepal.Length,4.6,4.6
+                4,setosa,Sepal.Width,Sepal.Length,3.1,4.6
+                4,setosa,Petal.Length,Sepal.Length,1.5,4.6
+                4,setosa,Petal.Width,Sepal.Length,0.2,4.6
+                4,setosa,Sepal.Length,Sepal.Width,4.6,3.1
+                4,setosa,Sepal.Width,Sepal.Width,3.1,3.1
+                4,setosa,Petal.Length,Sepal.Width,1.5,3.1
+                4,setosa,Petal.Width,Sepal.Width,0.2,3.1
+                4,setosa,Sepal.Length,Petal.Length,4.6,1.5
+                4,setosa,Sepal.Width,Petal.Length,3.1,1.5
+                4,setosa,Petal.Length,Petal.Length,1.5,1.5
+                4,setosa,Petal.Width,Petal.Length,0.2,1.5
+                4,setosa,Sepal.Length,Petal.Width,4.6,0.2
+                4,setosa,Sepal.Width,Petal.Width,3.1,0.2
+                4,setosa,Petal.Length,Petal.Width,1.5,0.2
+                4,setosa,Petal.Width,Petal.Width,0.2,0.2
+                5,setosa,Sepal.Length,Sepal.Length,5,5
+                5,setosa,Sepal.Width,Sepal.Length,3.6,5
+                5,setosa,Petal.Length,Sepal.Length,1.4,5
+                5,setosa,Petal.Width,Sepal.Length,0.2,5
+                5,setosa,Sepal.Length,Sepal.Width,5,3.6
+                5,setosa,Sepal.Width,Sepal.Width,3.6,3.6
+                5,setosa,Petal.Length,Sepal.Width,1.4,3.6
+                5,setosa,Petal.Width,Sepal.Width,0.2,3.6
+                5,setosa,Sepal.Length,Petal.Length,5,1.4
+                5,setosa,Sepal.Width,Petal.Length,3.6,1.4
+                5,setosa,Petal.Length,Petal.Length,1.4,1.4
+                5,setosa,Petal.Width,Petal.Length,0.2,1.4
+                5,setosa,Sepal.Length,Petal.Width,5,0.2
+                5,setosa,Sepal.Width,Petal.Width,3.6,0.2
+                5,setosa,Petal.Length,Petal.Width,1.4,0.2
+                5,setosa,Petal.Width,Petal.Width,0.2,0.2
+                6,setosa,Sepal.Length,Sepal.Length,5.4,5.4
+                6,setosa,Sepal.Width,Sepal.Length,3.9,5.4
+                6,setosa,Petal.Length,Sepal.Length,1.7,5.4
+                6,setosa,Petal.Width,Sepal.Length,0.4,5.4
+                6,setosa,Sepal.Length,Sepal.Width,5.4,3.9
+                6,setosa,Sepal.Width,Sepal.Width,3.9,3.9
+                6,setosa,Petal.Length,Sepal.Width,1.7,3.9
+                6,setosa,Petal.Width,Sepal.Width,0.4,3.9
+                6,setosa,Sepal.Length,Petal.Length,5.4,1.7
+                6,setosa,Sepal.Width,Petal.Length,3.9,1.7
+                6,setosa,Petal.Length,Petal.Length,1.7,1.7
+                6,setosa,Petal.Width,Petal.Length,0.4,1.7
+                6,setosa,Sepal.Length,Petal.Width,5.4,0.4
+                6,setosa,Sepal.Width,Petal.Width,3.9,0.4
+                6,setosa,Petal.Length,Petal.Width,1.7,0.4
+                6,setosa,Petal.Width,Petal.Width,0.4,0.4
+    """)))
+
+    record_spec = data_algebra.cdata.RecordSpecification(
+        control,
+        control_table_keys=['v1', 'v2'],
+        record_keys=['iris_id', 'Species'],
+    )
+    transform = RecordMap(blocks_out=record_spec)
+    res = transform.transform(iris)
+    assert data_algebra.test_util.equivalent_frames(res, expect)
