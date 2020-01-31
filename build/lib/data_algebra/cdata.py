29d60a6bd494c1d14c48d2551e7f6bf6e99d63a1
John Mount <jmount@win-vector.com>
2020-01-31 11:14:39 -0800

    start moving pivot to Pandas

--- build/lib/data_algebra/cdata.py
+++ build/lib/data_algebra/cdata.py
@@ -3,8 +3,9 @@ import collections
 
 import sqlite3
 
-import data_algebra
+import numpy
 
+import data_algebra
 import data_algebra.util
 import data_algebra.SQLite
 import data_algebra.data_ops
@@ -145,7 +146,7 @@ def record_spec_from_simple_obj(obj, *, pd=None):
     )
 
 
-def blocs_to_rowrecs(data, *, blocks_in, check_blocks_in_keying=True):
+def blocks_to_rowrecs(data, *, blocks_in):
     if not isinstance(blocks_in, data_algebra.cdata.RecordSpecification):
         raise TypeError("blocks_in should be a data_algebra.cdata.RecordSpecification")
     data = data.reset_index(drop=True)
@@ -155,14 +156,13 @@ def blocs_to_rowrecs(data, *, blocks_in, check_blocks_in_keying=True):
     )
     if len(missing_cols) > 0:
         raise KeyError("missing required columns: " + str(missing_cols))
-    if check_blocks_in_keying:
-        # table should be keyed by record_keys + control_table_keys
-        if not data_algebra.util.table_is_keyed_by_columns(
-            data, blocks_in.record_keys + blocks_in.control_table_keys
-        ):
-            raise ValueError(
-                "table is not keyed by blocks_in.record_keys + blocks_in.control_table_keys"
-            )
+    # table must be keyed by record_keys + control_table_keys
+    if not data_algebra.util.table_is_keyed_by_columns(
+        data, blocks_in.record_keys + blocks_in.control_table_keys
+    ):
+        raise ValueError(
+            "table is not keyed by blocks_in.record_keys + blocks_in.control_table_keys"
+        )
     # convert to row-records
     db_model = data_algebra.SQLite.SQLiteModel()
     x1_sql = x1_descr.to_sql(db_model)
@@ -173,32 +173,55 @@ def blocs_to_rowrecs(data, *, blocks_in, check_blocks_in_keying=True):
     return data
 
 
-def rowrecs_to_blocks(data, *, blocks_out, check_blocks_out_keying=False):
+def rowrecs_to_blocks(data, *, blocks_out, check_blocks_out_keying=False, pd=None):
     if not isinstance(blocks_out, data_algebra.cdata.RecordSpecification):
         raise TypeError("blocks_out should be a data_algebra.cdata.RecordSpecification")
+    if pd is None:
+        pd = data_algebra.pd
     data = data.reset_index(drop=True)
     x2_descr = data_algebra.data_ops.describe_table(data, table_name="x_blocks_out")
     missing_cols = set(blocks_out.record_keys) - set(x2_descr.column_names)
     if len(missing_cols) > 0:
         raise KeyError("missing required columns: " + str(missing_cols))
     if check_blocks_out_keying:
-        # table should be keyed by record_keys
+        # prefer table be keyed by record_keys
         if not data_algebra.util.table_is_keyed_by_columns(
             data, blocks_out.record_keys
         ):
             raise ValueError("table is not keyed by blocks_out.record_keys")
     # convert to block records
-    db_model = data_algebra.SQLite.SQLiteModel()
-    x2_sql = x2_descr.to_sql(db_model)
-    with sqlite3.connect(":memory:") as conn:
-        temp_table = data_algebra.data_ops.TableDescription(
-            "blocks_out", blocks_out.control_table.columns
-        )
-        db_model.insert_table(conn, data, x2_descr.table_name)
-        db_model.insert_table(conn, blocks_out.control_table, temp_table.table_name)
-        to_rows_sql = db_model.row_recs_to_blocks_query(x2_sql, blocks_out, temp_table)
-        data = db_model.read_query(conn, to_rows_sql)
-    return data
+    dtemp = data[blocks_out.record_keys + blocks_out.content_keys].copy()
+    dtemp.sort_values(by=blocks_out.record_keys, inplace=True)
+    dtemp = dtemp.reset_index(drop=True)
+    ctemp = blocks_out.control_table.copy()
+    dtemp['FALSE_JOIN_KEY'] = 1
+    ctemp['FALSE_JOIN_KEY'] = 1
+    res = dtemp[blocks_out.record_keys + ['FALSE_JOIN_KEY']].merge(ctemp, how='outer', on=['FALSE_JOIN_KEY'])
+    del res['FALSE_JOIN_KEY']
+    ckeys = blocks_out.control_table_keys
+    res.sort_values(by=blocks_out.record_keys + ckeys, inplace=True)
+    res = res.reset_index(drop=True)
+    del ctemp['FALSE_JOIN_KEY']
+    del dtemp['FALSE_JOIN_KEY']
+    value_keys = [k for k in ctemp.columns if k not in set(ckeys)]
+    donor_cols = set(dtemp.columns)
+    for vk in value_keys:
+        res[vk] = numpy.NaN
+    for i in range(ctemp.shape[0]):
+        want = numpy.ones((res.shape[0],), dtype=bool)
+        for ck in ckeys:
+            want = numpy.logical_and(want, res[ck] == ctemp[ck][i])
+        if numpy.any(want):
+            for vk in value_keys:
+                dcol = ctemp[vk][i]
+                if dcol in donor_cols:
+                    res.loc[want, vk] = numpy.asarray(dtemp[dcol])
+    # see about promoting composite columns to numeric
+    for vk in value_keys:
+        converted = pd.to_numeric(res[vk], errors='coerce')
+        if numpy.all(pd.isnull(converted) == pd.isnull(res[vk])):
+            res[vk] = converted
+    return res
 
 
 class RecordMap:
@@ -270,17 +293,16 @@ class RecordMap:
 
     # noinspection PyPep8Naming
     def transform(
-        self, X, *, check_blocks_in_keying=True, check_blocks_out_keying=False
+        self, X, *, check_blocks_out_keying=False
     ):
         unknown = set(self.columns_needed) - set(X.columns)
         if len(unknown) > 0:
             raise ValueError("missing required columns: " + str(unknown))
         X = X.reset_index(drop=True)
         if self.blocks_in is not None:
-            X = blocs_to_rowrecs(
+            X = blocks_to_rowrecs(
                 X,
                 blocks_in=self.blocks_in,
-                check_blocks_in_keying=check_blocks_in_keying,
             )
         if self.blocks_out is not None:
             X = rowrecs_to_blocks(
