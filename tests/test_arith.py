ad896bd2b92701497e1c949422f5ed166598fbd9
John Mount <jmount@win-vector.com>
2021-03-26 10:38:58 -0700

    add basic arithmetic test

---
+++ tests/test_arith.py
@@ -0,0 +1,37 @@
+
+import numpy
+import pandas
+
+from data_algebra.data_ops import *
+import data_algebra.util
+import data_algebra.test_util
+
+def test_arith_1():
+    d = pandas.DataFrame({
+        'x': [1, 2, 3, 4],
+        'y': [6, 9, 2, 8],
+    })
+
+    ops = describe_table(d, table_name='d'). \
+        extend({
+            'a': 'x + y',
+            'b': 'x - y',
+            'c': 'x * y',
+            'd': 'x / y',
+            'e': 'x + y / 2',
+            'f': 'x*x + y*y',
+            'g': '(x*x + y*y).sqrt()'
+        })
+
+    res = ops.transform(d)
+
+    expect = d.copy()
+    expect['a'] = expect.x + expect.y
+    expect['b'] = expect.x - expect.y
+    expect['c'] = expect.x * expect.y
+    expect['d'] = expect.x / expect.y
+    expect['e'] = expect.x + (expect.y / 2)
+    expect['f'] = (expect.x * expect.x) + (expect.y * expect.y)
+    expect['g'] = numpy.sqrt(((expect.x * expect.x) + (expect.y * expect.y)))
+
+    assert data_algebra.test_util.equivalent_frames(expect, res)
