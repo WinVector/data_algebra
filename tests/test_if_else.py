05bf1786e044ef51dbb52f533bc63caa3000cd78
John Mount <jmount@win-vector.com>
2021-03-26 14:06:46 -0700

    more tests

--- tests/test_if_else.py
+++ tests/test_if_else.py
@@ -1,6 +1,10 @@
+
+
 import sqlite3
 
-import data_algebra
+import numpy
+import pandas
+
 import data_algebra.test_util
 from data_algebra.data_ops import *
 from data_algebra.SQLite import SQLiteModel
@@ -36,3 +40,30 @@ def test_if_else():
     conn.close()
 
     assert data_algebra.test_util.equivalent_frames(res_db, expect)
+
+
+def test_if_else_2():
+    d = pandas.DataFrame({
+        'group': ['A', 'B', None, 'A', numpy.NAN, 'C'],
+        'c1': [1, 2, 3, 4, 5, 6],
+        'c2': [-1, -2, -3, -4, -5, -6],
+    })
+
+    ops = describe_table(d, table_name='d'). \
+        extend({'choice': "group=='A'"}). \
+        extend({'choice_fixed': 'choice.is_bad().if_else(0, choice)'}). \
+        extend({'rc': 'choice_fixed.if_else(c1, c2)'}). \
+        select_columns(['choice_fixed', 'rc'])
+
+    res1 = ops.transform(d)
+
+    db_model = SQLiteModel()
+
+    sql = ops.to_sql(db_model, pretty=True)
+
+    with sqlite3.connect(':memory:') as con:
+        db_model.prepare_connection(con)
+        d.to_sql(name='d', con=con)
+        res2 = pandas.read_sql(sql, con=con)
+
+    assert data_algebra.test_util.equivalent_frames(res1, res2)
