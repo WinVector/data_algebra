7eb790b4b8adde6bf7a98b4ba81e809a5e2a4c74
John Mount <jmount@win-vector.com>
2021-04-27 15:20:42 -0700

    check some expression shortening

--- tests/test_set_quoting.py
+++ tests/test_set_quoting.py
@@ -21,3 +21,21 @@ def test_set_quoting_1():
     sql = bq_handle.to_sql(ops, pretty=True)
     assert "'" not in sql
     assert '"' not in sql
+
+
+def test_set_quoting_2():
+    d = pandas.DataFrame({
+        'x': [1, -2, 3, -4]
+    })
+
+    bq_handle = data_algebra.BigQuery.BigQueryModel().db_handle(None)
+
+    ops = describe_table(d, table_name='d') .\
+        extend({
+            'select': f'x.is_in({-5, 1+2})'
+        })
+
+    sql = bq_handle.to_sql(ops, pretty=True)
+    assert "'" not in sql
+    assert '"' not in sql
+    assert '3' in sql
