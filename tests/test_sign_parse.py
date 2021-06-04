eda257b7871afb9bbfda738d18f8ac271eeb9808
John Mount <jmount@win-vector.com>
2021-06-04 15:03:36 -0700

    document where parsing is

--- tests/test_sign_parse.py
+++ tests/test_sign_parse.py
@@ -30,3 +30,15 @@ def test_sign_parse_1():
     #     'x_g': [1, -3, 3],
     # })
     # assert data_algebra.test_util.equivalent_frames(expect, res_pandas)
+
+
+def test_if_else_complex():
+    d = pandas.DataFrame({
+        'x': [1, 2, 3, 4, 5, 6],
+        'g': [1, 1, 1, 2, 2, 2],
+    })
+
+    # need to block this structure, as Pandas eval fails on it
+    with pytest.raises(ValueError):
+        ops = describe_table(d, table_name='d') .\
+            extend({'r': '(g>1).if_else(1, 2)'})
