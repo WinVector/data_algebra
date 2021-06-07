f9b4b012df95a07d4e42e6b36463964e355d876d
John Mount <jmount@win-vector.com>
2021-06-07 12:42:48 -0700

    add example rebuild and recheck

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ Recommended packages include: sqlparse, black, and graphviz.
 
 setuptools.setup(
     name='data_algebra',
-    version='0.7.0',
+    version='0.7.1',
     author='John Mount',
     author_email='jmount@win-vector.com',
     url='https://github.com/WinVector/data_algebra',
