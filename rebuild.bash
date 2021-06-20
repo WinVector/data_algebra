5b26dc1599137fb688cb5b564474665da8339c2d
John Mount <jmount@win-vector.com>
2021-06-20 10:03:05 -0700

    checkpoint before some code removal

--- rebuild.bash
+++ rebuild.bash
@@ -2,7 +2,7 @@
 bash ./clean.bash
 
 pytest --cov data_algebra tests > coverage.txt
-# pytest --cov-report term-missing --cov pkg/vtreat pkg/tests > coverage.txt
+# pytest --cov-report term-missing --cov data_algebra tests > coverage.txt
 pdoc -o ./docs ./data_algebra
 cat coverage.txt
 python3 setup.py sdist bdist_wheel
--- tests/test_OrderedSet.py
+++ tests/test_OrderedSet.py
@@ -3,9 +3,17 @@ import data_algebra.OrderedSet
 
 
 def test_OrderedSet_1():
-    expect = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6', 'x_7', 'x_8', 'x_9']
+    expect = ['x_0', 'x_1', 'x_2', 'x_3', 'x_5', 'x_6', 'x_7', 'x_8', 'x_9']
     s = data_algebra.OrderedSet.OrderedSet(expect)
     res = [v for v in s]
     assert expect == res
 
+    expect2 = expect.copy()
+    expect2.append('x_4')
+    s.update(['x_3', 'x_4'])
+    res2 = [v for v in s]
+    assert expect2 == res2
 
+    s.add('x_1')
+    res3 = [v for v in s]
+    assert expect2 == res3
