ee3d3d38af135a4458affc69fce1975c8dc1bbfc
John Mount <jmount@win-vector.com>
2021-07-04 07:05:29 -0700

    good run of Pandas, SQLite, PostgreSQL, and BigQuery

--- data_algebra/test_util.py
+++ data_algebra/test_util.py
@@ -332,7 +332,7 @@ def check_transform(
     # controls
     test_sqlite = True
     test_PostgreSQL = True
-    test_BigQuery = False
+    test_BigQuery = True
     # can't just add BigQuery until we set a default schema somehow
 
     # placeholders
