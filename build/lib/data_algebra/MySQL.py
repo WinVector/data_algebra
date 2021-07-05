283024b2a995c61aca43ab67fc27f83065b044a6
John Mount <jmount@win-vector.com>
2021-07-04 20:08:42 -0700

    rebuild and recheck

---
+++ build/lib/data_algebra/MySQL.py
@@ -0,0 +1,57 @@
+import data_algebra.data_ops
+import data_algebra.db_model
+
+
+def _MySQL_mean_expr(dbmodel, expression):
+    return (
+        "avg(" + dbmodel.expr_to_sql(expression.args[0], want_inline_parens=False) + ")"
+    )
+
+
+def _MySQL_size_expr(dbmodel, expression):
+    return "SUM(1)"
+
+def _MySQL_is_bad_expr(dbmodel, expression):
+    subexpr = dbmodel.expr_to_sql(expression.args[0], want_inline_parens=True)
+    return (
+        "("
+        + subexpr
+        + " IS NULL "   # TODO get infinity checks here
+        + " OR ("
+        + subexpr
+        + " != 0 AND "
+        + subexpr
+        + " = -"
+        + subexpr
+        + "))"
+    )
+
+# map from op-name to special SQL formatting code
+MySQL_formatters = {
+    "___": lambda dbmodel, expression: expression.to_python(),
+    "mean": _MySQL_mean_expr,
+    "size": _MySQL_size_expr,
+    "is_bad": _MySQL_is_bad_expr,
+}
+
+class MySQLModel(data_algebra.db_model.DBModel):
+    """A model of how SQL should be generated for MySQL.
+       Assuming we are using a sqlalhemy engine as our connection
+    """
+
+    def __init__(self):
+        data_algebra.db_model.DBModel.__init__(
+            self,
+            string_type='CHAR',
+            identifier_quote='`',
+            string_quote="'",
+            sql_formatters=MySQL_formatters,
+        )
+
+    def quote_identifier(self, identifier):
+        if not isinstance(identifier, str):
+            raise TypeError("expected identifier to be a str")
+        if self.identifier_quote in identifier:
+            # TODO: escape quotes
+            raise ValueError('did not expect ' + self.identifier_quote + ' in identifier')
+        return self.identifier_quote + identifier + self.identifier_quote
