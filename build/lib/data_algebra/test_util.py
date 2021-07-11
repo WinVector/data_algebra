e478f57903c407e52c95836799f47af48fc4a249
John Mount <jmount@win-vector.com>
2021-07-11 08:45:36 -0700

    rebuild and recheck

--- build/lib/data_algebra/test_util.py
+++ build/lib/data_algebra/test_util.py
@@ -18,10 +18,10 @@ from data_algebra.data_ops import *
 
 
 # controls
-test_PostgreSQL = False  # causes an external dependency
-test_BigQuery = False  # causes an external dependency
-test_MySQL = False  # causes an external dependency
-test_Spark = False  # causes an external dependency
+test_PostgreSQL = True  # causes an external dependency
+test_BigQuery = True  # causes an external dependency
+test_MySQL = True  # causes an external dependency
+test_Spark = True  # causes an external dependency
 
 
 def formats_to_self(ops):
