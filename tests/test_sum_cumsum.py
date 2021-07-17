1652a74e7726f2854e7cca697b97a78990ea3d01
John Mount <jmount@win-vector.com>
2021-07-17 12:40:19 -0700

    document (1).sum() / (1).cumsum() idioms

---
+++ tests/test_sum_cumsum.py
@@ -0,0 +1,62 @@
+
+import data_algebra
+from data_algebra.data_ops import *
+import data_algebra.util
+import data_algebra.test_util
+
+import pytest
+
+
+def test_sum_cumsum_1():
+    d = data_algebra.default_data_model.pd.DataFrame({
+        "x": [1.0, 2.0, 2.0, 1.0, 1.0],
+        "s": ['c', 'd', 'a', 'b', 'e'],
+         })
+
+    with pytest.raises(ValueError):
+        describe_table(d, table_name='d'). \
+            extend({
+                    'total_count': '(1).cumsum()',  # wrong method
+                },
+                )
+
+    with pytest.raises(ValueError):
+        describe_table(d, table_name='d'). \
+            extend({
+                    'total_order': '(1).sum()',  # wrong method
+                },
+                    order_by=['s'],
+                )
+
+    ops = describe_table(d, table_name='d') .\
+        extend({
+                'total_count': '(1).sum()',
+            },
+            ) . \
+        extend({
+                'group_count': '(1).sum()',
+            },
+            partition_by=['x'],
+            ) .\
+        extend({
+                    'total_order': '(1).cumsum()',
+            },
+            order_by=['s'],
+            ) .\
+        extend({
+                    'group_order': '(1).cumsum()',
+            },
+            partition_by=['x'],
+            order_by=['s'],
+            )
+
+    expect = data_algebra.default_data_model.pd.DataFrame({
+        'x': [1.0, 2.0, 2.0, 1.0, 1.0],
+        's': ['c', 'd', 'a', 'b', 'e'],
+        'total_count': [5, 5, 5, 5, 5],
+        'group_count': [3, 2, 2, 3, 3],
+        'total_order': [3, 4, 1, 2, 5],
+        'group_order': [2, 2, 1, 1, 3],
+        })
+
+    data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect)
