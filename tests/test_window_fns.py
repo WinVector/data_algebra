f0580ac8661bcab901dd92514dfe9fb1409a8939
John Mount <jmount@win-vector.com>
2021-07-17 11:12:49 -0700

    failing ngroup test

--- tests/test_window_fns.py
+++ tests/test_window_fns.py
@@ -1,4 +1,3 @@
-import sqlite3
 
 import data_algebra
 import data_algebra.test_util
@@ -111,3 +110,91 @@ def test_window_fns_pandas_only():
 
     res_pandas = ops.transform(d)
     assert data_algebra.test_util.equivalent_frames(res_pandas, expect1)
+
+
+def test_window_fns_project():
+    d = data_algebra.default_data_model.pd.DataFrame(
+        {
+            "g": [1, 2, 2, 3, 3, 3],
+            "x": [1, 4, 5, 7, 8, 9],
+            "v": [10, 40, 50, 70, 80, 90],
+        }
+    )
+
+    table_desciption = describe_table(d)
+    ops = table_desciption.extend(
+        {"row_number": "_row_number()", "shift_v": "v.shift()",},
+        order_by=["x"],
+        partition_by=["g"],
+    ).project(
+        {
+            # "ngroup": "_ngroup()",
+            "size": "_size()",
+            'size2': '(1).sum()',
+            "max_v": "v.max()",
+            "min_v": "v.min()",
+            "sum_v": "v.sum()",
+            "mean_v": "v.mean()",
+            "count_v": "v.count()",
+            "size_v": "v.size()",
+        },
+        group_by=["g"],
+    )
+
+    expect1 = data_algebra.default_data_model.pd.DataFrame({
+        'g': [1, 2, 3],
+        'size': [1, 2, 3],
+        'size2': [1, 2, 3],
+        'max_v': [10, 50, 90],
+        'min_v': [10, 40, 70],
+        'sum_v': [10, 90, 240],
+        'mean_v': [10, 45, 80],
+        'count_v': [1, 2, 3],
+        'size_v': [1, 2, 3],
+        })
+
+    data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect1)
+
+
+def test_window_fns_project_pandas_only():
+    d = data_algebra.default_data_model.pd.DataFrame(
+        {
+            "g": [1, 2, 2, 3, 3, 3],
+            "x": [1, 4, 5, 7, 8, 9],
+            "v": [10, 40, 50, 70, 80, 90],
+        }
+    )
+
+    table_desciption = describe_table(d)
+    ops = table_desciption.extend(
+        {"row_number": "_row_number()", "shift_v": "v.shift()",},
+        order_by=["x"],
+        partition_by=["g"],
+    ).project(
+        {
+            "ngroup": "_ngroup()",
+            "size": "_size()",
+            'size2': '(1).sum()',
+            "max_v": "v.max()",
+            "min_v": "v.min()",
+            "sum_v": "v.sum()",
+            "mean_v": "v.mean()",
+            "count_v": "v.count()",
+            "size_v": "v.size()",
+        },
+        group_by=["g"],
+    )
+
+    expect1 = data_algebra.default_data_model.pd.DataFrame({
+        'g': [1, 2, 3],
+        'size': [1, 2, 3],
+        'size2': [1, 2, 3],
+        'max_v': [10, 50, 90],
+        'min_v': [10, 40, 70],
+        'sum_v': [10, 90, 240],
+        'mean_v': [10, 45, 80],
+        'count_v': [1, 2, 3],
+        'size_v': [1, 2, 3],
+        })
+
+    data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect1)
