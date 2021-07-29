4b509ce7a9060044876b1b38eea76ca6beb038f0
John Mount <jmount@win-vector.com>
2021-07-28 19:20:39 -0700

    add test

---
+++ tests/test_compound_where.py
@@ -0,0 +1,37 @@
+import data_algebra
+import data_algebra.test_util
+from data_algebra.data_ops import *  # https://github.com/WinVector/data_algebra
+import data_algebra.util
+
+import data_algebra.SQLite
+
+
+def test_compount_where_and():
+    d = data_algebra.default_data_model.pd.DataFrame(
+        {
+            "a": ["a", "b", None, None],
+            "b": ["c", None, "d", None],
+            "x": [1, 2, None, None],
+            "y": [3, None, 4, None],
+        }
+    )
+
+    ops = (
+        describe_table(d, table_name='d')
+            .select_rows(
+                '(a == "a") & (b == "c") & (x > 0) & (y < 4)'
+            )
+        )
+
+    db_handle = data_algebra.SQLite.SQLiteModel().db_handle(conn=None)
+    sql = db_handle.to_sql(ops)
+    assert isinstance(sql, str)
+
+    expect = data_algebra.default_data_model.pd.DataFrame({
+        'a': ['a'],
+        'b': ['c'],
+        'x': [1.0],
+        'y': [3.0],
+        })
+
+    data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect)
