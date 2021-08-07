a4e5bfe1336b23d880b6075098962420c0a4f27d
John Mount <jmount@win-vector.com>
2021-08-07 09:46:31 -0700

    work on values work on db testing

--- tests/test_select_values.py
+++ tests/test_select_values.py
@@ -5,46 +5,22 @@ from data_algebra.data_ops import *  # https://github.com/WinVector/data_algebra
 import data_algebra.util
 import data_algebra.test_util
 
-import data_algebra.SQLite
-# import data_algebra.BigQuery
 
-
-def select_values_direct():
-    db_handle = data_algebra.SQLite.example_handle()
-    # db_handle = data_algebra.BigQuery.example_handle()
-
-    qi = db_handle.db_model.quote_identifier
-    qs = db_handle.db_model.quote_string
-    sql = f"""
-    SELECT 
-     {qi("column1")} AS {qi("c1")},
-     {qi("column2")} AS {qi("c2")}
-    FROM 
-     (VALUES 
-       ({qs('A')}, {qs('B')}), 
-       ({qs('C')}, {qs('D')}), 
-       ({qs('E')}, {qs('F')})
-     )
-    """
-    res = db_handle.read_query(sql)
-
-    expect = data_algebra.default_data_model.pd.DataFrame({
-        'c1': ['A', 'C', 'E'],
-        'c2': ['B', 'D', 'F'],
-        })
-
-    assert data_algebra.test_util.equivalent_frames(res, expect)
-
-
-def select_values_db_sqlite():
-    db_handle = data_algebra.SQLite.example_handle()
+def test_select_values_db_test_1():
+    db_handles = data_algebra.test_util.get_test_dbs()
 
     d = data_algebra.default_data_model.pd.DataFrame({
         'c1': ['A', 'C', 'E'],
-        'c2': ['B', 'D', 'F'],
+        'c2': [1, 2, 3],
         })
 
-    sql = db_handle.table_values_to_sql(d)
-    res = db_handle.read_query(sql)
+    for db_handle in db_handles:
+        # print(db_handle)
+        sql = db_handle.table_values_to_sql(d)
+        # print(sql)
+        res = db_handle.read_query(sql)
+        assert data_algebra.test_util.equivalent_frames(res, d)
 
-    assert data_algebra.test_util.equivalent_frames(res, d)
+    for db_handle in db_handles:
+        db_handle.close()
+
