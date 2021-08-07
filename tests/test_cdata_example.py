728569424303f54c314c3b54a81762dddbd89d3a
John Mount <jmount@win-vector.com>
2021-08-06 18:18:52 -0700

    rebuld and retest

--- tests/test_cdata_example.py
+++ tests/test_cdata_example.py
@@ -15,7 +15,7 @@ def test_cdata_example():
             "[ \\t]+",
             "",
             """
-    Sepal.Length,Sepal.Width,Petal.Length,Petal.Width,Species,id
+    Sepal_Length,Sepal_Width,Petal_Length,Petal_Width,Species,id
     5.1,3.5,1.4,0.2,setosa,0
     4.9,3.0,1.4,0.2,setosa,1
     4.7,3.2,1.3,0.2,setosa,2
@@ -30,7 +30,7 @@ def test_cdata_example():
         {
             "Part": ["Sepal", "Sepal", "Petal", "Petal"],
             "Measure": ["Length", "Width", "Length", "Width"],
-            "Value": ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"],
+            "Value": ["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"],
         }
     )
 
