f77b5d81eb823c5844e1d736dda14a29d369484a
John Mount <jmount@win-vector.com>
2021-08-07 09:09:43 -0700

    work on values queries

--- tests/test_select_values.py
+++ tests/test_select_values.py
@@ -6,39 +6,45 @@ import data_algebra.util
 import data_algebra.test_util
 
 import data_algebra.SQLite
+# import data_algebra.BigQuery
 
 
 def select_values_direct():
-   db_handle = data_algebra.SQLite.example_handle()
-   sql = """
-   SELECT 
-     "column1" AS "c1",
-     "column2" AS "c2"
-   FROM 
+    db_handle = data_algebra.SQLite.example_handle()
+    # db_handle = data_algebra.BigQuery.example_handle()
+
+    qi = db_handle.db_model.quote_identifier
+    qs = db_handle.db_model.quote_string
+    sql = f"""
+    SELECT 
+     {qi("column1")} AS {qi("c1")},
+     {qi("column2")} AS {qi("c2")}
+    FROM 
      (VALUES 
-       ('A', 'B'), 
-       ('C', 'D'), 
-       ('E', 'F'))
-   """
-   res = db_handle.read_query(sql)
+       ({qs('A')}, {qs('B')}), 
+       ({qs('C')}, {qs('D')}), 
+       ({qs('E')}, {qs('F')})
+     )
+    """
+    res = db_handle.read_query(sql)
 
-   expect = data_algebra.default_data_model.pd.DataFrame({
-    'c1': ['A', 'C', 'E'],
-    'c2': ['B', 'D', 'F'],
-    })
+    expect = data_algebra.default_data_model.pd.DataFrame({
+        'c1': ['A', 'C', 'E'],
+        'c2': ['B', 'D', 'F'],
+        })
 
-   assert data_algebra.test_util.equivalent_frames(res, expect)
+    assert data_algebra.test_util.equivalent_frames(res, expect)
 
 
 def select_values_db_sqlite():
-   db_handle = data_algebra.SQLite.example_handle()
+    db_handle = data_algebra.SQLite.example_handle()
 
-   d = data_algebra.default_data_model.pd.DataFrame({
-    'c1': ['A', 'C', 'E'],
-    'c2': ['B', 'D', 'F'],
-    })
+    d = data_algebra.default_data_model.pd.DataFrame({
+        'c1': ['A', 'C', 'E'],
+        'c2': ['B', 'D', 'F'],
+        })
 
-   sql = db_handle.table_values_to_sql(d)
-   res = db_handle.read_query(sql)
+    sql = db_handle.table_values_to_sql(d)
+    res = db_handle.read_query(sql)
 
-   assert data_algebra.test_util.equivalent_frames(res, d)
+    assert data_algebra.test_util.equivalent_frames(res, d)
