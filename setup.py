1bbc71fc9ee4a3911d256978da41af4e6096bea0
John Mount <jmount@win-vector.com>
2021-08-30 08:17:27 -0700

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ the [rquery](https://github.com/WinVector/rquery) and
 
 setuptools.setup(
     name='data_algebra',
-    version='0.7.9',
+    version='0.8.0',
     author='John Mount',
     author_email='jmount@win-vector.com',
     url='https://github.com/WinVector/data_algebra',
