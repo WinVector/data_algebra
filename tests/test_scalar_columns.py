04dae8f0710898fc280bb4d2d4e7ab22a5dd65bc
John Mount <jmount@win-vector.com>
2021-09-23 14:09:13 -0700

    bump version add failing test on adding scalar columns

---
+++ tests/test_scalar_columns.py
@@ -0,0 +1,33 @@
+
+import numpy
+
+import data_algebra
+from data_algebra.data_ops import *
+import data_algebra.test_util
+
+
+def test_scalar_columns():
+    d = data_algebra.default_data_model.pd.DataFrame(
+        {"x": [1, 2, 3, 4]}
+    )
+
+    ops = (
+        describe_table(d, table_name="d")
+            .extend({
+                'z1': numpy.nan,
+                'z2': None,
+                'n1': 1,
+                'c1': 'a',
+            })
+    )
+    res = ops.transform(d)
+
+    expect = d.copy()
+    expect['z1'] = numpy.nan
+    expect['z2'] = None
+    expect['n1'] = 1
+    expect['c1'] = 'a'
+
+    assert data_algebra.test_util.equivalent_frames(res, expect)
+
+    data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect)
