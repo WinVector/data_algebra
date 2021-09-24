ccaeaf808195ac077656323f4fef3281c80a3664
John Mount <jmount@win-vector.com>
2021-09-24 07:25:30 -0700

    better document column scaler init tests intent

--- tests/test_scalar_columns.py
+++ tests/test_scalar_columns.py
@@ -17,9 +17,11 @@ def test_scalar_columns():
                 'n1': 1,
                 'z1': numpy.nan,
                 'z2': None,
-                'c1': "'a'",
+                'z3': 'None',  # none
+                'c1': "'a'",   # string
                 'b1': True,
                 'f1': 2.1,
+                'f2': '3.5',   # number
             })
     )
     res = ops.transform(d)
@@ -28,12 +30,15 @@ def test_scalar_columns():
     expect['n1'] = 1
     expect['z1'] = numpy.nan
     expect['z2'] = None
+    expect['z3'] = None
     expect['c1'] = 'a'
     expect['b1'] = True
     expect['f1'] = 2.1
+    expect['f2'] = 3.5
 
     assert data_algebra.test_util.equivalent_frames(res, expect)
 
-    # TODO: turn on check_parse
-    # TODO: turn on DB checks
+    # Note: leaving parse and db checks off as this pipeline
+    # wasn't formed purely from text and nan/None/logical is handled
+    # differently in dbs.
     # data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect, check_parse=False)
