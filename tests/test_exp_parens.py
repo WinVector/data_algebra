0fec47a5909ac7fdbe700a14a3125b6f90a2ffd6
John Mount <jmount@win-vector.com>
2021-09-29 09:29:50 -0700

    add failing test on double paren issue

---
+++ tests/test_exp_parens.py
@@ -0,0 +1,16 @@
+import numpy
+
+import data_algebra
+import data_algebra.test_util
+import data_algebra.util
+from data_algebra.data_ops import *
+
+
+def test_exp_parens():
+    d_local = data_algebra.default_data_model.pd.DataFrame({'x': [1, 2, 3]})
+    ops = (
+        data(d_local)
+        .extend({'p': '(x+1).exp()'})
+    )
+    ops_str = str(ops)
+    assert '((' not in ops_str
