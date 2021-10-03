1ab19a8ae72a9b67e69a50b7097a921e751c1f5b
John Mount <jmount@win-vector.com>
2021-10-03 11:52:22 -0700

    copy example into tests

---
+++ tests/test_compare_data_frames.py
@@ -0,0 +1,80 @@
+
+# import packages
+import string
+import numpy
+import numpy.random
+
+import data_algebra
+from data_algebra.data_ops import *
+from data_algebra.cdata import *
+import data_algebra.test_util
+import data_algebra.util
+
+
+def test_compare_data_frames():
+    # from https://github.com/WinVector/data_algebra/blob/main/Examples/GettingStarted/comparing_two_dataframes.ipynb
+    # build synthetic example data
+
+    # seed the pseudo-random generator for repeatability
+    numpy.random.seed(1999)
+
+    # choose our simulated number of observations
+    n_obs = 100
+    symbols = list(string.ascii_lowercase)
+
+    d1 = data_algebra.default_data_model.pd.DataFrame({
+        'group': numpy.random.choice(symbols, size=n_obs, replace=True),
+    })
+
+    d2 = data_algebra.default_data_model.pd.DataFrame({
+        'group': numpy.random.choice(symbols, size=n_obs, replace=True),
+    })
+
+    # which columns we consider to be row keys
+    # can be more than one column
+    grouping_columns = ['group']
+
+    summary_ops = (
+        descr(d1=d1)
+            .project(
+                {'d1_count': '(1).sum()'},
+                group_by=grouping_columns)
+            .natural_join(
+                b=descr(d2=d2)
+                    .project(
+                        {'d2_count': '(1).sum()'},
+                        group_by=grouping_columns),
+                by=grouping_columns,
+                jointype='full')
+            .extend({
+                'd1_count': 'd1_count.coalesce(0)',
+                'd2_count': 'd2_count.coalesce(0)',
+                })
+    )
+
+    summary_table = summary_ops.eval({'d1': d1, 'd2': d2})
+
+    res = ex(
+        data(summary_table)
+            .select_rows('(d1_count <= 0) | (d2_count <= 0)')
+            .order_rows(grouping_columns)
+    )
+
+    expect = data_algebra.default_data_model.pd.DataFrame({
+        'group': ['u', 'w'],
+        'd1_count': [0.0, 4.0],
+        'd2_count': [3.0, 0.0],
+        })
+
+    assert data_algebra.test_util.equivalent_frames(res, expect)
+
+    # ops2 = (
+    #     summary_ops
+    #         .select_rows('(d1_count <= 0) | (d2_count <= 0)')
+    #         .order_rows(grouping_columns)
+    # )
+    # # TODO: emulate right and full join in SQLite
+    # # https://www.sqlitetutorial.net/sqlite-full-outer-join/
+    # data_algebra.test_util.check_transform(
+    #     ops=ops2, data={"d1": d1, "d2": d2}, expect=expect
+    # )
