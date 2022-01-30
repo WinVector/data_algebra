4b0ebe4c849d5a07b1fe963ba2c61f0de2544daa
John Mount <jmount@win-vector.com>
2022-01-30 13:25:37 -0800

    bump version rebuild and retest add CTE elim example

--- data_algebra/__init__.py
+++ data_algebra/__init__.py
@@ -3,7 +3,7 @@ Package for data processing in Python: https://github.com/WinVector/data_algebra
 """
 
 __docformat__ = "restructuredtext"
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 
 __doc__ = """
 `data_algebra`<https://github.com/WinVector/data_algebra> is a piped data wrangling system
