8fc3d23e1e03787d8613bbad62c11f4d2fa62059
John Mount <jmount@win-vector.com>
2022-04-11 07:47:55 -0700

    update how non-warning test is handled

--- tests/test_join_effects.py
+++ tests/test_join_effects.py
@@ -1,3 +1,5 @@
+
+import warnings
 import pytest
 
 import data_algebra
@@ -19,10 +21,12 @@ def test_join_warn1():
         b=describe_table(d_right, table_name="d_right"), by="k", jointype="LEFT"
     )
 
+    # https://stackoverflow.com/a/45671804/6901725
+    # https://github.com/pytest-dev/pytest/issues/9404#issue-1076710891
     # https://stackoverflow.com/questions/45671803/how-to-use-pytest-to-assert-no-warning-is-raised
-    with pytest.warns(None) as record:
-        res = ops.eval({"d_left": d_left, "d_right": d_right})
-        assert len(record) == 0
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        ops.eval({"d_left": d_left, "d_right": d_right})
 
     expect = data_algebra.default_data_model.pd.DataFrame(
         {"k": ["a", "a", "b"], "x": [1.0, None, 3.0], "y": [1.0, 10.0, 20.0],}
