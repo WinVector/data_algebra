326cb8feb10afe9416f1750cc49b217d4f8e4a6c
John Mount <jmount@win-vector.com>
2022-11-19 11:39:42 -0800

    clean up frame creation path special case out zero-row extend

--- tests/test_agg.py
+++ tests/test_agg.py
@@ -8,13 +8,19 @@ def test_agg():
     d = data_algebra.pandas_model.default_data_model.pd.DataFrame(
         {"x": [1, 2, 3, 4], "g": [1, 1, 2, 2],}
     )
-
     ops = describe_table(d, table_name="d").project({"x": "x.max()"}, group_by=["g"])
-
     expect = data_algebra.pandas_model.default_data_model.pd.DataFrame({"g": [1, 2], "x": [2, 4],})
-
     db_handle = data_algebra.SQLite.SQLiteModel().db_handle(conn=None)
     sql = db_handle.to_sql(ops)
     assert isinstance(sql, str)
-
     data_algebra.test_util.check_transform(ops=ops, data=d, expect=expect)
+
+
+def test_agg_0_cols():
+    d = data_algebra.pandas_model.default_data_model.pd.DataFrame(
+        {"x": [], "g": [],}
+    )
+    ops = describe_table(d, table_name="d").project({"x": "x.max()"}, group_by=["g"])
+    res = ops.transform(d)
+    cols_seen = set(res.columns)
+    assert cols_seen == set(["g", "x"])
