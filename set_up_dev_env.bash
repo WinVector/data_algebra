5bf53464c14c73d5b981819b4e1f8cc198aac523
John Mount <jmount@win-vector.com>
2022-11-23 13:41:42 -0800

    run more in example move comment in set up script

--- set_up_dev_env.bash
+++ set_up_dev_env.bash
@@ -3,6 +3,7 @@
 conda remove --name data_algebra_dev_env --all --yes
 conda env create -f data_algebra_dev_env.yaml
 conda activate data_algebra_dev_env
-pip install --no-deps -e "$(pwd)"  # sym link to source files
+# sym link to source files
+pip install --no-deps -e "$(pwd)"  
 
 
