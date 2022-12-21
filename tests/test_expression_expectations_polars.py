089864686c9476db2ae9a06f5ac8f5c157c8313f
John Mount <jmount@win-vector.com>
2022-12-21 07:35:29 -0800

    clean up test a bit

--- tests/test_expression_expectations_polars.py
+++ tests/test_expression_expectations_polars.py
@@ -17,59 +17,51 @@ except ModuleNotFoundError:
 
 
 def test_expression_expectations_polars_1():
-    known_not_to_work = set([
-        'arctan2', 
-        'base_Sunday',
-        'bfill',
-        'concat',
-        'count',  # Pandas null treatment in the wrong
-        'cumcount',
-        'date_diff',
-        'datetime_to_date',
-        'dayofmonth',
-        'dayofweek',
-        'dayofyear',
-        'expm1',
-        'ffill',
-        'format_date',
-        'format_datetime',
-        'is_in',
-        'log1p',
-        'month',
-        'mapv',
-        'nunique',
-        'parse_date',
-        'quarter',
-        'shift',
-        'std',
-        'timestamp_diff',
-        'trimstr',
-        'var',
-        'weekofyear',
-        'year',
-        '_ngroup',
-        ])
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    # https://github.com/WinVector/data_algebra/blob/main/Examples/Methods/data_algebra_catalog.ipynb 
-    expectation_path = os.path.join(dir_path, "expr_expectations.pkl.gz")
-    with gzip.open(expectation_path, 'rb') as in_f:
-        expectation_map = pickle.load(in_f)
-    d = expectation_map['d']
-    d_polars = None
     if have_polars:
+        known_not_to_work = set([
+            'arctan2', 
+            'base_Sunday',
+            'bfill',
+            'concat',
+            'count',  # Pandas null treatment in the wrong
+            'cumcount',
+            'date_diff',
+            'datetime_to_date',
+            'dayofmonth',
+            'dayofweek',
+            'dayofyear',
+            'expm1',
+            'ffill',
+            'format_date',
+            'format_datetime',
+            'is_in',
+            'log1p',
+            'month',
+            'mapv',
+            'nunique',
+            'parse_date',
+            'quarter',
+            'shift',
+            'std',
+            'timestamp_diff',
+            'trimstr',
+            'var',
+            'weekofyear',
+            'year',
+            '_ngroup',
+            ])
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        # https://github.com/WinVector/data_algebra/blob/main/Examples/Methods/data_algebra_catalog.ipynb 
+        expectation_path = os.path.join(dir_path, "expr_expectations.pkl.gz")
+        with gzip.open(expectation_path, 'rb') as in_f:
+            expectation_map = pickle.load(in_f)
+        d = expectation_map['d']
         d_polars = pl.DataFrame(d)
-    expectations = expectation_map['expectations']
-    for op, op_class, exp, ops, expect in expectations:
-        if data_algebra.data_model.default_data_model().is_appropriate_data_instance(expect):
-            # test Pandas
-            res = ops.transform(d)
-            if not data_algebra.data_model.default_data_model().is_appropriate_data_instance(res):
-                raise ValueError(f"failure on {op} {op_class} {exp} {ops}")
-            if not  data_algebra.test_util.equivalent_frames(res, expect):
-                raise ValueError(f"failure on {op} {op_class} {exp} {ops}")
-            if op not in known_not_to_work:
-                # test Polars
-                if have_polars:
+        expectations = expectation_map['expectations']
+        for op, op_class, exp, ops, expect in expectations:
+            if data_algebra.data_model.default_data_model().is_appropriate_data_instance(expect):
+                if op not in known_not_to_work:
+                    # test Polars
                     res_polars = ops.transform(d_polars)
                     if not isinstance(res_polars, pl.DataFrame):
                         raise ValueError(f"failure on {op} {op_class} {exp} {ops}")
