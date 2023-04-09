3423fabfd7cac4cfa8ae03469eb882aeac8dc1c1
John Mount <jmount@win-vector.com>
2023-04-08 18:17:28 -0700

    rebuild and retest

--- build/lib/data_algebra/pandas_base.py
+++ build/lib/data_algebra/pandas_base.py
@@ -547,7 +547,6 @@ class PandasModelBase(data_algebra.data_model.DataModel, data_algebra.expression
         """
         assert op.node_name == "SQLNode"
         db_handle = data_map[op.view_name]
-        # would like (but causes circular import) assert isinstance(db_handle, data_algebra.db_model.DBHandle)
         res = db_handle.read_query("\n".join(op.sql))
         res = self.data_frame(res[op.columns_produced()])
         assert self.is_appropriate_data_instance(res)
