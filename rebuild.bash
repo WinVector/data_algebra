57e450943e607df6d4ddcc547e4a81ae8dfcde63
John Mount <jmount@win-vector.com>
2023-09-27 16:04:45 -0600

    list pkgs on rebuild

--- rebuild.bash
+++ rebuild.bash
@@ -1,12 +1,10 @@
 
 bash ./clean.bash
-
-# pytest --cov data_algebra tests > coverage.txt
 pip install --no-deps -e "$(pwd)"
+conda list --export > data_algebra_dev_env_package_list.txt
 pytest --cov-report term-missing --cov data_algebra tests > coverage.txt
 pdoc3 --force -o ./docs ./data_algebra
 cat coverage.txt
 python3 setup.py sdist bdist_wheel
-# pip install dist/data_algebra-*.tar.gz
 twine check dist/*
 
